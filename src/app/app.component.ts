ef65a2f6cd0bdb626dab35365fd77acb96bfcbed
Javi Molina <jmolina@mysphera.com>
2022-06-19 13:27:27 +0200

    feat(logo): add app main logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { CustomBreakpointObserver } from './layout';
 
 @Component({
   selector: 'app-root',
   template: `
     <header>
-      <img src="assets/aTrendsPRO.svg" alt="Logo Avantio Trends PRO" />
+      <img
+        *ngIf="isSmallScreen$ | async"
+        class="app-logo"
+        src="assets/aTrendsPRO.svg"
+        alt="Logo Avantio Trends PRO"
+      />
       <app-menu></app-menu>
     </header>
     <main>
       <router-outlet></router-outlet>
     </main>
   `,
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  isSmallScreen$ = this.breakpointsObserver.isSmall$;
+
+  constructor(private breakpointsObserver: CustomBreakpointObserver) {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutModule } from './layout';
 import { AppMenuModule } from './menu';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AppMenuModule],
+  imports: [BrowserModule, AppRoutingModule, AppLayoutModule, AppMenuModule],
   providers: [],
   bootstrap: [AppComponent],
 })
