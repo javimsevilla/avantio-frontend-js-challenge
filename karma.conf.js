93503bb855020a8a195fc362c08d0d5866aea408
Javi Molina <jmolina@mysphera.com>
2022-06-19 19:33:16 +0200

    test(viewport): install karma-viewport plugin

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,14 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular', 'viewport'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-viewport'),
     ],
     client: {
       jasmine: {
@@ -19,18 +20,18 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/avantio-frontend-js-challenge'),
+      dir: require('path').join(
+        __dirname,
+        './coverage/avantio-frontend-js-challenge'
+      ),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -39,6 +40,32 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    // Viewport configuration
+    viewport: {
+      breakpoints: [
+        {
+          name: 'small-screen',
+          size: {
+            width: 320,
+            height: 480,
+          },
+        },
+        {
+          name: 'medium-screen',
+          size: {
+            width: 768,
+            height: 1024,
+          },
+        },
+        {
+          name: 'large-screen',
+          size: {
+            width: 1440,
+            height: 900,
+          },
+        },
+      ],
+    },
   });
 };
