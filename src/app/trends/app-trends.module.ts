dd679bf666597d6e58fe8da97744b873f5a3673b
Javi Molina <jmolina@mysphera.com>
2022-06-20 20:36:20 +0200

    chore(trends): add routing module

--- src/app/trends/app-trends.module.ts
+++ src/app/trends/app-trends.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrendsListComponent } from './trends-list/trends-list.component';
+import { AppTrendsRoutingModule } from './app-trends-routing.module';
 
 @NgModule({
   declarations: [TrendsListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AppTrendsRoutingModule],
   exports: [TrendsListComponent],
 })
 export class AppTrendsModule {}
