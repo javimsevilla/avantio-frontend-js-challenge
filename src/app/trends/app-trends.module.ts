a6fa5623b8f532e42e151e0aa6c33a17eed983d7
Javi Molina <jmolina@mysphera.com>
2022-06-21 18:56:53 +0200

    chore(trends): add effect to obtain all trends

--- src/app/trends/app-trends.module.ts
+++ src/app/trends/app-trends.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { TrendsListComponent } from './trends-list/trends-list.component';
 import { AppTrendsRoutingModule } from './app-trends-routing.module';
 import { trendsFeatureKey, trendsReducer } from './store/reducers';
+import { trendsEffects } from './store/effects';
 import { TrendService } from './trend.service';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './auth-interceptor';
@@ -15,6 +17,7 @@ import { AuthInterceptor } from './auth-interceptor';
     AppTrendsRoutingModule,
     HttpClientModule,
     StoreModule.forFeature(trendsFeatureKey, trendsReducer),
+    EffectsModule.forFeature(trendsEffects),
   ],
   exports: [TrendsListComponent],
   providers: [
