d05ada151c77a0a8ea4ed66ea19c90eb75a5e5d4
Javi Molina <jmolina@mysphera.com>
2022-06-22 12:26:55 +0200

    feat(trends): add trends list

--- src/app/trends/trends-list/trends-list.component.spec.ts
+++ src/app/trends/trends-list/trends-list.component.spec.ts
@@ -1,22 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { loadTrends } from '../store/actions/trends-list-page.actions';
+import { selectAllTrends } from '../store/selectors';
 
 import { TrendsListComponent } from './trends-list.component';
 
 describe('TrendsListComponent', () => {
   let component: TrendsListComponent;
   let fixture: ComponentFixture<TrendsListComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TrendsListComponent],
+      providers: [provideMockStore({})],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TrendsListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
+    fixture = TestBed.createComponent(TrendsListComponent);
+    component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should load trends on init', () => {
+    fixture = TestBed.createComponent(TrendsListComponent);
+    spyOn(store, 'dispatch').and.callFake(() => {});
+    selectAllTrends.setResult([
+      {
+        id: 'id-test-1',
+        title: 'title-test-1',
+        body: ['body-test-1-p1'],
+        provider: 'elpais',
+        image: 'image-test-1',
+        url: 'url-test-1',
+        createdAt: new Date(),
+      },
+    ]);
+
+    fixture.detectChanges();
+    const trends = fixture.debugElement.queryAll(By.css('.trend'));
+
+    expect(store.dispatch).toHaveBeenCalledOnceWith(loadTrends());
+    expect(trends.length).toBe(1);
   });
 });
--- src/app/trends/trends-list/trends-list.component.ts
+++ src/app/trends/trends-list/trends-list.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { loadTrends } from '../store/actions/trends-list-page.actions';
+import { selectAllTrends } from '../store/selectors';
 
 @Component({
   selector: 'app-trends-list',
-  template: ` <p>trends-list works!</p> `,
-  styles: [],
+  template: `
+    <article class="trend" *ngFor="let trend of trends$ | async; index as i">
+      <figure class="trend__figure">
+        <img class="trend__image" [src]="trend.image" [alt]="trend.title" />
+        <figcaption class="trend__title">
+          <h2>{{ trend.title }}</h2>
+        </figcaption>
+      </figure>
+      <p class="trend__excerpt">{{ trend.body[0] }}</p>
+    </article>
+  `,
+  styleUrls: ['./trends-list.component.scss'],
 })
-export class TrendsListComponent {}
+export class TrendsListComponent implements OnInit {
+  protected trends$ = this.store.select(selectAllTrends);
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.store.dispatch(loadTrends());
+  }
+}
