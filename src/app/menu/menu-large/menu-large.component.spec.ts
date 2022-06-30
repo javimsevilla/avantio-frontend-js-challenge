5a6e21c8bac6ba1a5ef69ff7fb4158e1248fdda4
Javi Molina <jmolina@mysphera.com>
2022-06-30 15:22:55 +0200

    test(menu-large): fix spec module dependency

--- src/app/menu/menu-large/menu-large.component.spec.ts
+++ src/app/menu/menu-large/menu-large.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { MenuLargeComponent } from './menu-large.component';
 
@@ -11,6 +12,7 @@ describe('MenuLargeComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [MenuLargeComponent],
       imports: [RouterTestingModule],
+      providers: [provideMockStore()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MenuLargeComponent);
